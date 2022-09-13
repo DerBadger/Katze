f1790fa2fb8aa616bb2e59bf5d921caa009dd503
DerBadger <112956503+DerBadger@users.noreply.github.com>
2022-09-13 10:47:45 +0200

    Add files via upload
    Erste Version

---
+++ MavenPorj/src/main/java/TestPackage/MavenPorj/App.java
@@ -0,0 +1,10 @@
+package TestPackage.MavenPorj;
+
+public class App {
+
+
+
+    public static void main(String[] args) {
+        appReferences.launch(appReferences.class);
+    }
+}
---
+++ MavenPorj/src/main/java/TestPackage/MavenPorj/SecondaryController.java
@@ -0,0 +1,12 @@
+package TestPackage.MavenPorj;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        appReferences.setRoot("primary");
+    }
+}
